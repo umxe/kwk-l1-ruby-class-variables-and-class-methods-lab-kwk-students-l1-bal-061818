8ff8f5a1083d1b078304f414a550b8dbb354c7f2
umxe <u_habiba@rocketmail.com>
2018-06-25 14:13:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,23 @@
-
+class Song 
+  @@count = 0
+  @@genres = []
+  attr :name, :artist, :genre 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    count += 1
+  end
+  
+  def genre
+    return @@genres
+  end
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.artist
+    return @artist
+  end
+end
